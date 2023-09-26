ae486a9813ecad0d8a0353b658e88c12b4d0c12a
amritkuikel <63916236+amritkuikel@users.noreply.github.com>
2023-09-26 22:29:23 +0545

    authcomplete4

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,22 +4,12 @@ export default function middleware(req: NextRequest) {
   let verify = req.cookies.get("token");
   let url = req.url;
   if (verify && url.includes("/login")) {
-    return NextResponse.redirect(
-      "http://localhost:3000/" || "https://attendies-eight.vercel.app"
-    );
+    return NextResponse.redirect("https://attendies-eight.vercel.app");
   }
-  if (
-    !verify &&
-    url === ("http://localhost:3000/" || "https://attendies-eight.vercel.app")
-  ) {
-    return NextResponse.redirect(
-      "http://localhost:3000/login" ||
-        "https://attendies-eight.vercel.app/login"
-    );
+  if (!verify && url === "https://attendies-eight.vercel.app") {
+    return NextResponse.redirect("https://attendies-eight.vercel.app/login");
   }
   if (verify && url.includes("/register")) {
-    return NextResponse.redirect(
-      "http://localhost:3000/" || "https://attendies-eight.vercel.app"
-    );
+    return NextResponse.redirect("https://attendies-eight.vercel.app");
   }
 }
