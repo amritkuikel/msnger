8ecfbf640127b7567ba8d89e25d2cb7754aa8bcb
amritkuikel <63916236+amritkuikel@users.noreply.github.com>
2023-09-27 12:10:18 +0545

    authcompleteforreal2

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 export default function middleware(req: NextRequest) {
   let verify = req.cookies.get("token");
   let url = req.url;
-  // if (verify && url.includes("/login")) {
-  //   return NextResponse.redirect("https://attendies-eight.vercel.app/dashboard");
-  // }
-  // if (!verify && url.includes("/dashboard" )) {
-  //   return NextResponse.redirect("https://attendies-eight.vercel.app/login");
-  // }
-  // if (verify && url.includes("/register")) {
-  //   return NextResponse.redirect("https://attendies-eight.vercel.app/dashboard");
-  // }
+  if(url.includes('https://attendies-eight.vercel.app')){
+    if (verify && url.includes("/login")) {
+      return NextResponse.redirect("https://attendies-eight.vercel.app/dashboard");
+    }
+    if (!verify && url.includes("/dashboard" )) {
+      return NextResponse.redirect("https://attendies-eight.vercel.app/login");
+    }
+    if (verify && url.includes("/register")) {
+      return NextResponse.redirect("https://attendies-eight.vercel.app/dashboard");
+    }
+  }
   if (verify && url.includes("/login")) {
     return NextResponse.redirect("http://localhost:3000/dashboard");
   }
