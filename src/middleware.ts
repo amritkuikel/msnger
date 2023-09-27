eea3cdb67be69abe1f28be42ed1c83026e118c2a
amritkuikel <63916236+amritkuikel@users.noreply.github.com>
2023-09-27 10:53:09 +0545

    authcomplete6

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
   let verify = req.cookies.get("token");
-  console.log(verify)
   let url = req.url;
   if (verify && url.includes("/login")) {
     return NextResponse.redirect("https://attendies-eight.vercel.app");
   }
-  if (!verify && url === "https://attendies-eight.vercel.app") {
+  if (!verify && url.startsWith("https://attendies-eight.vercel.app" )) {
     return NextResponse.redirect("https://attendies-eight.vercel.app/login");
   }
   if (verify && url.includes("/register")) {
